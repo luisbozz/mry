31e612c08de34ee1c715b7ec6bb513f8dc8b59d4
luisbozz <47217513+luisbozz@users.noreply.github.com>
2022-07-25 11:56:21 +0200

    Projektdateien hinzufügen.

---
+++ mry/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("mry")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mry")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+[assembly: Obfuscation(Feature = "merge with System.Runtime.CompilerServices.Unsafe.dll", Exclude = false)]
+[assembly: Obfuscation(Feature = "encrypt resources [compress]", Exclude = false)]
+[assembly: Obfuscation(Feature = "sanitize resources [minify]", Exclude = false)]
+[assembly: Obfuscation(Feature = "code control flow obfuscation", Exclude = false)]
+[assembly: Obfuscation(Feature = "rename serializable symbols", Exclude = false)]
+[assembly: Obfuscation(Feature = "encrypt symbol names with password gglrg6hF5P6WijQalBjUl4TFRYZAeGv9mUcxU4aCbBSn1SG8uJ22jLul7GPE0HZX", Exclude = false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("c893c51e-779c-49eb-ae22-a79360db5a7c")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
